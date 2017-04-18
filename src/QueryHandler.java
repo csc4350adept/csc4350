a539e0a35e6d1f24f565c3a381210a5d3a8beeb9
ebull1985 <ebull@knights.ucf.edu>
2017-04-17 21:37:06 -0400

    QueryHandler getPassword()

--- src/QueryHandler.java
+++ src/QueryHandler.java
@@ -35,23 +35,41 @@ public class QueryHandler {
 	 * Retrieves a password from the database using a username
 	 */
 	public static String getPassword (String username) {
+		//Gets database connection "c"
 		java.sql.Connection c;
 		try {
 			c = createDB();
 		} catch (SQLException e) {
 			return null;
 		}
-		String sql = String.format("select passwordhash from users where email = '%s'", username);
+		//Constructs SQL string
+		String sql = String.format("select passwordhash from users where email='%s'", username);
+		//Executes query
 		try {
-			Statement msg = c.createStatement();
-			ResultSet resp = msg.executeQuery(sql);
-			if (resp.getString("passwordhash") != null) return resp.getString("passwordhash");
+			Statement st = c.createStatement();
+			ResultSet rs = st.executeQuery(sql);
+			System.out.println("Executed query: " + sql);
+			String pword;
+			//If there is a row returned, get the column "passwordhash" and return it
+			if (rs.next() && rs.getString("passwordhash") != null) {
+				pword = rs.getString("passwordhash");
+				System.out.println(String.format("Got this password from the DB: %s", rs.getString("passwordhash")));
+				return pword;
+			}
 		} catch (SQLException e) {
 			/* nothing */
 		}
+		//If there was a SQLException or no password, return null
 		return null;
 	}
 	
+	
+	/*
+	 * Take in a user name
+	 * Return an ArrayList<String> of mailbox names that belong to that user
+	 * Modify email table so that every email belongs to a folder
+	 * Create table mailboxes with columns <email, mailboxName> with both as primary keys 
+	 */
 	public static ArrayList listRef(String userName) {
 		ArrayList<String> folders = new ArrayList<String>();
 		folders.add("inbox");
@@ -61,6 +79,10 @@ public class QueryHandler {
 		return folders; // The user's actual folders in the DB
 	}
 	
+	/*
+	 * Take in a email and a mailboxName
+	 * Return an ArrayList of emailIDs that belong to that email and belong to that mailboxName
+	 */
 	public static ArrayList listMailbox(String userName, String mailboxName) {
 		ArrayList<String> emailIDs = new ArrayList<String>();
 		emailIDs.add("1");
@@ -69,6 +91,10 @@ public class QueryHandler {
 		return emailIDs; // The user's actual email IDs in the DB
 	}
 	
+	/*
+	 * Take in a email and emailID
+	 * Return a HashMap<String, String> with all the data for the email that has that emailID and belongs to that email
+	 */
 	public static HashMap emailID(String userName, String emailID) {
 		HashMap<String, String> email = new HashMap<String, String>();
 		email.put("owner", "ebull");
@@ -77,6 +103,9 @@ public class QueryHandler {
 		return email; // The user's actual email data corresponding to this.emailID, in the DB
 	}
 	
+	/*
+	 * We probably don't need this method
+	 */
 	public static boolean updateAll(HashMap<String, HashMap<String, String>> mailboxEmails) {
 		return true;
 	}
